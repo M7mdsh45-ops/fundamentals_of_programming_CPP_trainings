db1cbe1c339a15943eea23eacc0c7f5788dac3e6
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-04-22 09:10:16 +0200

    Create print_asterisk.cpp

---
+++ print_asterisk.cpp
@@ -0,0 +1,20 @@
+// print_asterisk.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 10 ; i >= 1 ; i --)
+    {
+        for (int j = 1 ; j <= i ; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+
+    }
+
+
+
+}
