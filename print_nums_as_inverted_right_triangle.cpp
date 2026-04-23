3327ecb804c22c4a95a1bdfb293d036496f99b47
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-04-23 02:37:36 +0200

    Create print_nums_as_inverted_right_triangle.cpp

---
+++ print_nums_as_inverted_right_triangle.cpp
@@ -0,0 +1,26 @@
+// Print_the numbers_in_an_inverted_right_triangle.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 10 ; i >= 1 ; i--)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			
+			
+			cout << j << " ";
+
+		}
+		cout << endl;
+    }
+
+
+
+
+
+}
+
